496196d229aa87f03a1a86423d81534f36e4a6b3
Anton <spectral307@gmail.com>
2023-01-18 14:26:49 +0300

    Disable too-few-public-methods err for MainWindow

--- plotter/main_window.py
+++ plotter/main_window.py
@@ -4,6 +4,8 @@ from PyQt6.QtWidgets import QMainWindow
 from .ui_main_window import Ui_MainWindow
 
 
+# Reason: more public methods will be added later.
+# pylint: disable-next=too-few-public-methods
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
