309ea93b8265881c91e0655ee76c5c96fe571bfd
Anton <spectral307@gmail.com>
2023-01-18 17:01:39 +0300

    Open QFileDialog on open_files action triggered

--- plotter/main_window.py
+++ plotter/main_window.py
@@ -1,6 +1,6 @@
 # Reason: PyQt6 is a third party module.
 # pylint: disable-next=no-name-in-module
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QFileDialog, QMainWindow
 from .ui_main_window import Ui_MainWindow
 
 
@@ -12,7 +12,17 @@ class MainWindow(QMainWindow):
         self.__ui = Ui_MainWindow()
         self.__ui.setupUi(self)
 
+        self.__ui.open_files.triggered.connect(self.open_files)
         self.__ui.exit_app.triggered.connect(self.exit_app)
 
     def exit_app(self):
         QApplication.quit()
+
+    def open_files(self):
+        caption = "Открыть файлы"
+        directory = "/"
+        filefilter = "Все файлы (*.*)"
+        # Reason: the variable will be used later.
+        # pylint: disable-next=unused-variable
+        filenames, _ = QFileDialog.getOpenFileNames(
+            self, caption, directory, filefilter)
