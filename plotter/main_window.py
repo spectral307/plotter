217bf0baf359325700df937266c1f8b49e0b9257
Anton <spectral307@gmail.com>
2023-01-20 10:03:05 +0300

    Enable too-few-public-methods check for MainWindow

--- plotter/main_window.py
+++ plotter/main_window.py
@@ -8,8 +8,6 @@ from matplotlib.backends.backend_qtagg import FigureCanvas
 from .ui_main_window import Ui_MainWindow
 
 
-# Reason: more public methods will be added later.
-# pylint: disable-next=too-few-public-methods
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
