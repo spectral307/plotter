803446b419e3480f9bce1a5cb9e44e7817c3030a
Anton <spectral307@gmail.com>
2023-01-23 16:32:56 +0300

    Add "default_dir" and "previous_dir" to QSettings

--- plotter/__main__.py
+++ plotter/__main__.py
@@ -1,6 +1,9 @@
 import sys
 # Reason: PyQt6 is a third party module.
 # pylint: disable-next=no-name-in-module
+from PyQt6.QtCore import QSettings
+# Reason: PyQt6 is a third party module.
+# pylint: disable-next=no-name-in-module
 from PyQt6.QtWidgets import QApplication
 from .main_window import MainWindow
 
@@ -11,6 +14,12 @@ def main():
     app.setApplicationName("plotter")
     app.setOrganizationName("GTLab")
     app.setOrganizationDomain("gtlab.pro")
+
+    settings = QSettings()
+    if not settings.value("default_dir"):
+        settings.setValue("default_dir", "/")
+    if not settings.value("previous_dir"):
+        settings.setValue("previous_dir", settings.value("default_dir"))
 
     main_window = MainWindow()
     main_window.show()
