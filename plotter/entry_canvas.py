fa6a9a74771d7eefbe666ce634e19fef44284ea3
Anton <spectral307@gmail.com>
2023-01-27 14:53:15 +0300

    Add canvas related operations to EntryCanvas

--- plotter/entry_canvas.py
+++ plotter/entry_canvas.py
@@ -1,9 +1,23 @@
+from matplotlib.figure import Figure
 # Reason: matplotlib is a third party module.
 # pylint: disable-next=no-name-in-module
 from matplotlib.backends.backend_qtagg import FigureCanvas
 
 
-# Reason: more public methods will be added later.
-# pylint: disable-next=too-few-public-methods
 class EntryCanvas(FigureCanvas):
-    pass
+    def __init__(self):
+        super().__init__(Figure())
+        self.__axes = self.figure.subplots()
+        self.__axes.grid()
+        self.__lines = []
+
+    def plot(self, *args, **kwargs):
+        line, = self.__axes.plot(*args, **kwargs)
+        self.__lines.append(line)
+        self.draw_idle()
+
+    def clear(self):
+        for line in self.__lines:
+            self.__axes.lines.remove(line)
+        self.__lines.clear()
+        self.draw_idle()
