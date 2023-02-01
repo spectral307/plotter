a14327b5f8e83cf5b3f95aad72194f8dced01fd5
Anton <spectral307@gmail.com>
2023-02-01 17:13:15 +0300

    Introduce GtlAfcReportEntry and Header classes

---
+++ plotter/gtl_afc_report_entry.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from .header import Header
+
+
+# Reason: more public methods will be added later.
+# pylint: disable-next=too-few-public-methods
+class GtlAfcReportEntry:
+    x_header = Header("Frequency", "F", "E", "Hz")
+    y_headers = (Header("Sensitivity absolute", "S(abs)", "F"),
+                 Header("Sensitivity relative", "S(rel)", "G", "%"))
+
+    def __init__(self, name: str, data: pd.DataFrame):
+        self.name = name
+        self.data = data
+
+    def get_xy_data(self, y_header: Header) -> tuple[pd.Series, pd.Series]:
+        if y_header not in GtlAfcReportEntry.y_headers:
+            raise ValueError(f"No such header in entry: {y_header}.")
+        return self.data[GtlAfcReportEntry.x_header], self.data[y_header]
---
+++ plotter/header.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass(frozen=True)
+class Header:
+    name: str
+    short_name: str
+    excel_col: str
+    unit: Optional[str] = None
+    description: Optional[str] = None
+
+    def __repr__(self) -> str:
+        ret = self.short_name
+        if self.unit is not None:
+            ret += f", {self.unit}"
+        return ret
