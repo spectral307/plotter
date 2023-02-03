60b91ee0075e5281eabb003abd00ee0dfcef7f81
Anton <spectral307@gmail.com>
2023-02-03 16:07:55 +0300

    Add FrequencyResponse.from_excel class method

--- plotter/frequency_response.py
+++ plotter/frequency_response.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from os.path import basename, splitext
 from .data_header import DataHeader
 from .entry import Entry
 
@@ -13,6 +14,33 @@ class FrequencyResponse(Entry):
     def __init__(self, name: str, data: pd.DataFrame):
         self.name = name
         self.data = data
+
+    @classmethod
+    def from_excel(self, file: str, sheet: str,
+                   column_mapping: dict[DataHeader, str], name=None) \
+            -> "FrequencyResponse":
+        file_basename = basename(file)
+        root, ext = splitext(file_basename)
+
+        if ext != ".xlsx" or ext != ".xlsm":
+            raise ValueError(f"excel file expected, got '{ext}'")
+
+        if name is None:
+            name = root
+
+        usecols = column_mapping[FrequencyResponse.x_header]
+        names = [FrequencyResponse.x_header]
+        for header in FrequencyResponse.y_headers:
+            usecols += f",{column_mapping[header]}"
+            names.append(header)
+
+        data = pd.read_excel(
+            file,
+            sheet_name=sheet,
+            usecols=usecols,
+            names=names)
+
+        return FrequencyResponse(name, data)
 
     def get_xy_data(self, y_header: DataHeader) -> tuple[pd.Series, pd.Series]:
         if y_header not in FrequencyResponse.y_headers:
