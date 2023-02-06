5f80cb2729f01a8c7c91d0b3225778398d08a753
Anton <spectral307@gmail.com>
2023-02-06 14:00:18 +0300

    Move methods from FrequencyResponse to Entry

--- plotter/entry.py
+++ plotter/entry.py
@@ -1,2 +1,44 @@
+from os.path import basename, splitext
+import pandas as pd
+from .data_header import DataHeader
+
+
 class Entry:
-    pass
+    x_header: DataHeader
+    y_headers: tuple[DataHeader]
+
+    def __init__(self, name: str, data: pd.DataFrame):
+        self.name = name
+        self.data = data
+
+    @classmethod
+    def from_excel(self, file: str, sheet: str,
+                   column_mapping: dict[DataHeader, str], name=None) \
+            -> "Entry":
+        file_basename = basename(file)
+        root, ext = splitext(file_basename)
+
+        if ext != ".xlsx" or ext != ".xlsm":
+            raise ValueError(f"Excel file expected, got: {ext}")
+
+        if name is None:
+            name = root
+
+        usecols = column_mapping[Entry.x_header]
+        names = [Entry.x_header]
+        for header in Entry.y_headers:
+            usecols += f",{column_mapping[header]}"
+            names.append(header)
+
+        data = pd.read_excel(
+            file,
+            sheet_name=sheet,
+            usecols=usecols,
+            names=names)
+
+        return Entry(name, data)
+
+    def get_xy_data(self, y_header: DataHeader) -> tuple[pd.Series, pd.Series]:
+        if y_header not in Entry.y_headers:
+            raise ValueError(f"y_header isn't in entry: {y_header}")
+        return self.data[Entry.x_header], self.data[y_header]
--- plotter/frequency_response.py
+++ plotter/frequency_response.py
@@ -1,5 +1,3 @@
-import pandas as pd
-from os.path import basename, splitext
 from .data_header import DataHeader
 from .entry import Entry
 
@@ -7,42 +5,8 @@ from .entry import Entry
 # Reason: more public methods will be added later.
 # pylint: disable-next=too-few-public-methods
 class FrequencyResponse(Entry):
-    x_header = DataHeader("Frequency", "F", "Hz")
-    y_headers = (DataHeader("Sensitivity absolute", "S(abs)"),
-                 DataHeader("Sensitivity relative", "S(rel)", "%"))
-
-    def __init__(self, name: str, data: pd.DataFrame):
-        self.name = name
-        self.data = data
-
-    @classmethod
-    def from_excel(self, file: str, sheet: str,
-                   column_mapping: dict[DataHeader, str], name=None) \
-            -> "FrequencyResponse":
-        file_basename = basename(file)
-        root, ext = splitext(file_basename)
-
-        if ext != ".xlsx" or ext != ".xlsm":
-            raise ValueError(f"Excel file expected, got: {ext}")
-
-        if name is None:
-            name = root
-
-        usecols = column_mapping[FrequencyResponse.x_header]
-        names = [FrequencyResponse.x_header]
-        for header in FrequencyResponse.y_headers:
-            usecols += f",{column_mapping[header]}"
-            names.append(header)
-
-        data = pd.read_excel(
-            file,
-            sheet_name=sheet,
-            usecols=usecols,
-            names=names)
-
-        return FrequencyResponse(name, data)
-
-    def get_xy_data(self, y_header: DataHeader) -> tuple[pd.Series, pd.Series]:
-        if y_header not in FrequencyResponse.y_headers:
-            raise ValueError(f"y_header isn't in entry: {y_header}")
-        return self.data[FrequencyResponse.x_header], self.data[y_header]
+    x_header: DataHeader = DataHeader("Frequency", "F", "Hz")
+    y_headers: tuple[DataHeader] = (
+        DataHeader("Sensitivity absolute", "S(abs)"),
+        DataHeader("Sensitivity relative", "S(rel)", "%")
+    )
