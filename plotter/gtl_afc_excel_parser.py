63951d817b90c26a8848357efdfe11d346d2cbbf
Anton <spectral307@gmail.com>
2023-02-22 13:45:56 +0300

    Implement GtlAfcExcelParser class methods

--- plotter/gtl_afc_excel_parser.py
+++ plotter/gtl_afc_excel_parser.py
@@ -1,10 +1,16 @@
 from .entry import Entry
 from .parser import Parser
+from .frequency_response import FrequencyResponse
 
 
 class GtlAfcExcelParser(Parser):
     def parse_file(self, file: str) -> list[Entry]:
-        raise NotImplementedError()
+        column_mapping = {
+            FrequencyResponse.x_header: "E",
+            FrequencyResponse.y_headers[0]: "F",
+            FrequencyResponse.y_headers[1]: "G"
+        }
+        return [FrequencyResponse.from_excel(file, "data", column_mapping)]
 
     def parse_files(self, files: list[str]) -> list[Entry]:
-        raise NotImplementedError()
+        return [self.parse_file(file)[0] for file in files]
