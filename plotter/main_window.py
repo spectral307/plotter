9c4c6bccbfb882bec4647b100a9685e77988464d
Anton <spectral307@gmail.com>
2023-02-28 14:50:06 +0300

    Display and toggle entries in MainWindow

--- plotter/main_window.py
+++ plotter/main_window.py
@@ -7,6 +7,8 @@ from PyQt6.QtCore import QSettings
 # pylint: disable-next=no-name-in-module
 from PyQt6.QtWidgets import QApplication, QFileDialog, QMainWindow
 from .entry_canvas import EntryCanvas
+from .frequency_response import FrequencyResponse
+from .gtl_afc_excel_parser import GtlAfcExcelParser
 from .ui_main_window import Ui_MainWindow
 
 
@@ -23,6 +25,10 @@ class MainWindow(QMainWindow):
         self.__ui.canvas = EntryCanvas()
 
         self.__ui.splitter.replaceWidget(1, self.__ui.canvas)
+
+        self.__entry_type = FrequencyResponse
+        self.__parser = GtlAfcExcelParser()
+        self.__entries = {}
 
     def exit_app(self):
         QApplication.quit()
@@ -43,6 +49,16 @@ class MainWindow(QMainWindow):
         if new_directory != directory:
             settings.setValue("last_dir", new_directory)
 
+        self.__entries.clear()
+        entries = self.__parser.parse_files(files)
+        for entry in entries:
+            self.__entries[entry.name] = entry
+        self.__ui.entries.set_entries([entry.name for entry in entries])
+        self.__ui.canvas.set_entries(entries, self.__entry_type.y_headers[0])
+        self.__ui.canvas.display_all_entries()
+        self.__ui.entries.entry_toggled.connect(self.__handle_entry_toggled)
+        self.__ui.canvas.set_y_header(self.__entry_type.y_headers[1])
+
     # Reason: the method will be decomposed later.
     # pylint: disable-next=too-many-locals
     def open_folder(self):
@@ -55,10 +71,29 @@ class MainWindow(QMainWindow):
             return
 
         pathname = join(folder, "*.*")
-        # Reason: the variable will be used later.
-        # pylint: disable-next=unused-variable
-        files = glob(pathname)  # noqa: F841
+        files = glob(pathname)
 
         new_directory = dirname(folder)
         if new_directory != directory:
             settings.setValue("last_dir", new_directory)
+
+        self.__entries.clear()
+        entries = self.__parser.parse_files(files)
+        for entry in entries:
+            self.__entries[entry.name] = entry
+
+        self.__entries.clear()
+        entries = self.__parser.parse_files(files)
+        for entry in entries:
+            self.__entries[entry.name] = entry
+        self.__ui.entries.set_entries([entry.name for entry in entries])
+        self.__ui.canvas.set_entries(entries, self.__entry_type.y_headers[0])
+        self.__ui.canvas.display_all_entries()
+        self.__ui.entries.entry_toggled.connect(self.__handle_entry_toggled)
+        self.__ui.canvas.set_y_header(self.__entry_type.y_headers[1])
+
+    def __handle_entry_toggled(self, entryname, checked):
+        if checked:
+            self.__ui.canvas.display_entry(entryname)
+        else:
+            self.__ui.canvas.hide_entry(entryname)
